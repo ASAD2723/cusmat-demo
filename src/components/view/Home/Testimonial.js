a26fc2e61e428abfa3605d3c4b2aa875cbe7e3bf
tajwarsultan <tajwar.sultan00@gmail.com>
2022-06-02 01:19:41 +0500

    the code is merged

--- src/components/view/Home/Testimonial.js
+++ src/components/view/Home/Testimonial.js
@@ -25,7 +25,7 @@ const testimonialCards = [
     name: "DTDC",
     review:
       "This DTDC app is relly good. With this app, a new biker can learn the whole process of delivery by his/her own without any help.",
-  }
+  },
 ];
 
 const Testimonial = () => {
@@ -66,54 +66,80 @@ const Testimonial = () => {
   };
   return (
     <>
-      
       <section className="custom-container testimonial-sec">
-      <br />
-      <h2 className="H2" style={{fontSize:"32px"}}>Testimonials</h2>
+        <br />
+        <h2 className="H2" style={{ fontSize: "32px" }}>
+          Testimonials
+        </h2>
 
-      <div className="px-2" style={{height:"auto"}}>
-        <Slider {...settings}>
-          
+        <div className="px-2" style={{ height: "auto" }}>
+          <Slider {...settings}>
             <div className="testimonial-slider1 text-center pt-2">
-              
               <div className="py-xl-5 py-4 mb-8">
-                <h4 className="H4" style={{fontSize:"18px"}}>We never thought we could have a simulator for winding engine operator training. Young engineers from Cusmat really understood the machine and problem well.</h4>
+                <h4 className="H4" style={{ fontSize: "18px" }}>
+                  We never thought we could have a simulator for winding engine
+                  operator training. Young engineers from Cusmat really
+                  understood the machine and problem well.
+                </h4>
               </div>
               <br />
               <br />
-              <img src="/images/home/icons.svg" alt="" className=" img-fluid mx-auto testimonial-img" style={{paddingTop:"35px", paddingBottom:"10px"}} />
-              <small className="" style={{paddingTop:"-15px"}}>MOIL Limited</small>
+              <img
+                src="/images/home/icons.svg"
+                alt=""
+                className=" img-fluid mx-auto testimonial-img"
+                style={{ paddingTop: "35px", paddingBottom: "10px" }}
+              />
+              <small className="" style={{ paddingTop: "-15px" }}>
+                MOIL Limited
+              </small>
             </div>
 
             <div className="testimonial-slider2 text-center pt-2">
-              
               <div className="py-xl-5 py-4 mb-8">
-                <h4 className="H4" style={{fontSize:"18px"}}>Ausra Health has been working with cusmat for creation of a mobile app. The app helps the healthcare workers to understand various parts of, equipment assembly and onboarding of patients. Ausra is very happy with his collabration.</h4>
+                <h4 className="H4" style={{ fontSize: "18px" }}>
+                  Ausra Health has been working with cusmat for creation of a
+                  mobile app. The app helps the healthcare workers to understand
+                  various parts of, equipment assembly and onboarding of
+                  patients. Ausra is very happy with his collabration.
+                </h4>
               </div>
               <br />
-              <img src="/images/home/Frame.svg" alt="" className=" img-fluid mx-auto testimonial-img"  style={{paddingBottom:"10px"}}/>
+              <img
+                src="/images/home/Frame.svg"
+                alt=""
+                className=" img-fluid mx-auto testimonial-img"
+                style={{ paddingBottom: "10px" }}
+              />
               <small className="">Ausra Healthcare</small>
             </div>
 
             <div className="testimonial-slider3 text-center pt-2">
-              
               <div className="py-xl-5 py-4 mb-8">
-                <h4 className="H4" style={{fontSize:"18px"}}>This DTDC app is relly good. With this app, a new biker can learn the whole process of delivery by his/her own without any help.</h4>
+                <h4 className="H4" style={{ fontSize: "18px" }}>
+                  This DTDC app is relly good. With this app, a new biker can
+                  learn the whole process of delivery by his/her own without any
+                  help.
+                </h4>
               </div>
               <br />
               <br />
               <br />
               <br />
-              <img src="/images/home/Frame (1).svg" alt="" className=" img-fluid mx-auto testimonial-img"  style={{paddingTop:"10px",paddingBottom:"15px"}} />
+              <img
+                src="/images/home/Frame (1).svg"
+                alt=""
+                className=" img-fluid mx-auto testimonial-img"
+                style={{ paddingTop: "10px", paddingBottom: "15px" }}
+              />
               <small className="">DTDC</small>
             </div>
-          
-        </Slider>
-        <br />
-        <br />
-        <br />
-      </div>
-    </section>
+          </Slider>
+          <br />
+          <br />
+          <br />
+        </div>
+      </section>
       <Footer />
     </>
   );
